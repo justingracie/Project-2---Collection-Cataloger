33b976f1bf72767f590f489edcf6b00e3637c8b5
justingracie <j.m.gracie@gmail.com>
2022-04-26 12:03:00 -0600

    Dev direcorty setup, ReadMe Edit

---
+++ models/Catalog.js
@@ -0,0 +1,54 @@
+//models/Discog
+
+const mongoose = require('mongoose');
+
+const catalogSchema = new mongoose.Schema({
+    album:{
+        type: String,
+        required: [true, 'ablum cannot be empty :(']
+    },
+    artist: {
+        type: String, 
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    itemValue: {
+        type: Number,
+        min: [0, 'you cannot add a negative number'],
+        required: true
+    },
+    mediaFormat: {
+        type: String, 
+
+    },
+    realeaseYear:{
+        type: String
+    },
+    trackList:{
+        type: [String],
+        required: true
+    },
+    collectorComments:{
+        type: String
+    },
+    spotifyLink: {
+        type: String, 
+        required: false
+    },
+    appleMusic: {
+        type: String,
+        required: false
+    }
+},
+    {
+        timestamps: true
+    }
+
+);
+
+const Catalog = mongoose.model('Discog', catalogSchema);
+
+module.exports = Catalog;
