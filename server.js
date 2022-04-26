2526d0932aa9271209189edb03f5f5fe9b3fcbee
justingracie <j.m.gracie@gmail.com>
2022-04-26 14:03:30 -0600

    MongoBD connection established

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 // const methodOverride = require('method-override');
-// const catalogController = require('/controllers/catalog_controller');
-// const reviewController = require('/controllers/review_controller');
+// const catalogController = require('./controllers/catalog_controller');
+// const reviewController = require('./controllers/review_controller');
 
 // app.set('view engine', 'ejs');
 
