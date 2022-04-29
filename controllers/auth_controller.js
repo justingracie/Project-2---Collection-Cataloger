ebc49ec7e0696cb12d09da0c9abed6dbd1f92be0
justingracie <j.m.gracie@gmail.com>
2022-04-29 10:57:19 -0600

    registration post route adding user to DB

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -3,15 +3,37 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
-// Register route ---->
-router.get('/register', function (req,res){
+// Register route (presentational) ---->
+router.get('/register', (req,res) => {
     return res.render('auth/register');
 });
 
-// Login Route Present login---->
+// Login Route (presentational)---->
 
-router.get('/login', function(req, res){
+router.get('/login', (req, res) =>{
     res.render('auth/login');
+});
+
+//Register router (functional)---->
+
+router.post('/register', async (req, res, next) => {
+    try{
+        console.log('testing')
+        const foundUser = await User.exists({email: req.body.email});
+        if(foundUser){
+            return res.redirect('/login');
+        }
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt)
+        const hash = await bcrypt.hash(req.body.password, salt);
+        console.log(hash)
+        req.body.password = hash;
+        const newUser = await User.create(req.body);
+        return res.redirect('/login');
+    }catch (error){
+        console.log(error);
+        return res.send(error);
+    }
 });
 
 
