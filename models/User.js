e15d766e4f299ad5183906dc202423b5a9fd27cd
justingracie <j.m.gracie@gmail.com>
2022-04-28 19:28:52 -0600

    User Schema has been created

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String,
+        required: true, 
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add your email address'],
+        unique: true,
+    },
+    password: {
+        type: String, 
+        required: true,
+    }, 
+    avatar: {
+        type: String, 
+        default: 'https://images.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/12/20/Pictures/_0745b0ec-231b-11ea-8c10-7db3e225203f.jpg',
+    },
+},
+{
+    timestamps: true,
+}
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
